79c6df891c48c15e6cd819c200a427b071ef538c
Gilles Coissac <gilles@ideographe.fr>
2024-05-13 17:20:12 +0200

    chore: remove unused files

--- {{cookiecutter.project_name}}/docs/source/index.md
+++
@@ -1,9 +0,0 @@
-# Welcome to {{cookiecutter.project_name}}'s documentation!
-
-``` {toctree}
-:maxdepth: 2
-:caption: "Contents:"
-
-
-```
-
---
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,9 @@
+"""{{ cookiecutter.package_name|capitalize() }} package.
+
+{{ cookiecutter.short_description }}
+"""
+
+from ._version import __version__
+
+
+__all__ = ["__version__"]
